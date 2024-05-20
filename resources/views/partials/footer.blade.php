8dfb9bb24cecb2409b47e589c023335a4310af9a
Aligern <sasso.luca93@gmail.com>
2024-05-20 19:39:11 +0200

    added style to header and footerTop

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <div id="footerTop" class="text-white">
         <div class="container">
         @foreach ( $footerLinks as $link )
-            <div id="box" class="d-flex justify-content-between">
+            <div id="box" class="d-flex">
                 <img src="{{ $link['img'] }}" alt="Logos">
                     <a href="#">
                         {{ $link['text'] }}
